2c25cab521336b73d444b2c54003f116d8f441ce
javieradel <ashair455@gmail.com>
2025-09-06 14:05:57 -0300

    chore(app): backend scaffold

---
+++ App/src/test/java/EventosChetos/EventosChetos/EventosChetosApplicationTests.java
@@ -0,0 +1,13 @@
+package EventosChetos.EventosChetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosChetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
